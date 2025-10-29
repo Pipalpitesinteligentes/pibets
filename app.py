25f5505d534408c254d0484a25a19217a40d03ca
Pipalpitesinteligentes <felipesouzacontatoo@gmail.com>
2025-10-29 14:36:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ if getp("key") == APP_INTERNAL_KEY:
 
 # ======================== Config visual / CSS ========================
 st.set_page_config(page_title="Palpite Inteligente", page_icon="⚽", layout="wide")
+st.set_page_config(page_title="Palpite Inteligente", page_icon="⚽", layout="wide", initial_sidebar_state="expanded")
 HIDE_TOOLBAR = """
 <style>
 /* Toolbar e rodapé */
@@ -271,6 +272,7 @@ if is_admin:
             st.info("Envie este código ao assinante.")
 
 # ======================== FIM ========================
+
 
 
 
